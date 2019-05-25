13a9fc2e981ccfc9e9e8e72fd2963392331b618c
sunmoon <756973628@qq.com>
2019-05-25 21:43:48 +0800

    链表练习

---
+++ linkedlist/DoublyLinkedList2.java
@@ -0,0 +1,234 @@
+package linkedlist;
+
+/**
+ * 双向链表（哨兵模式）
+ * <p>
+ * 在插入和删除元素时，不用考虑边界问题。
+ *
+ * @param <E>
+ * @author wangzk
+ */
+public class DoublyLinkedList2<E>
+{
+    private int size;
+    /**
+     * 指向第一个元素虚拟节点
+     */
+    private Node<E> header;
+    /**
+     * 后接最后一个元素虚拟节点
+     */
+    private Node<E> tailer;
+
+    public DoublyLinkedList2()
+    {
+        header = new Node<>(null, null, null);
+        tailer = new Node<>(null, null, null);
+        header.next = tailer;
+        tailer.prev = header;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    /**
+     * 前插一个元素
+     *
+     * @param e 待插元素
+     */
+    public void addAsHead(E e)
+    {
+        Node<E> head = header.next;
+        Node<E> newNode = new Node<>(header, e, head);
+        header.next = newNode;
+        head.prev = newNode;
+        size++;
+    }
+
+    /**
+     * 追加一个元素
+     *
+     * @param e 待追加元素
+     */
+    public void addAsTail(E e)
+    {
+        Node<E> tail = tailer.prev;
+        Node<E> newNode = new Node<>(tail, e, tailer);
+        tailer.prev = newNode;
+        tail.next = newNode;
+        size++;
+    }
+
+    /**
+     * 在指定的位置插入新元素
+     *
+     * @param e   待插元素
+     * @param idx 待插入位置
+     */
+    public void addByIdx(E e, int idx) throws Exception
+    {
+        if (idx > size || idx < 1)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        // 找到指定位置上的节点
+        Node<E> node = getNode(idx);
+        // 创建新的节点
+        Node<E> newNode = new Node<>(node.prev, e, node);
+        // 更新指针
+        node.prev.next = newNode;
+        node.prev = newNode;
+
+        size++;
+    }
+
+    /**
+     * 删除第一个元素
+     *
+     * @return 被删元素
+     * @throws Exception 链表已空
+     */
+    public E removeHead() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+
+        Node<E> head = header.next;
+        Node<E> node = head.next;
+
+        header.next = node;
+        node.prev = header;
+
+        size--;
+        return head.item;
+    }
+
+    /**
+     * 删除最后一个元素
+     *
+     * @return 被删元素
+     * @throws Exception 链表已空
+     */
+    public E removeTail() throws Exception
+    {
+        if (isEmpty())
+        { throw new Exception("链表已空！"); }
+
+        Node<E> tail = tailer.prev;
+        Node<E> node = tail.prev;
+
+        tailer.prev = node;
+        node.next = tailer;
+
+        size--;
+        return tail.item;
+    }
+
+    /**
+     * 删除指定位置的元素
+     *
+     * @param idx 待删元素的索引
+     * @return 被删元素
+     */
+    public E removeByIdx(int idx) throws Exception
+    {
+        if (idx > size || idx < 1)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        // 找到待删节点
+        Node<E> removedNode = getNode(idx);
+        // 更新前后节点的指针
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+
+        size--;
+        return removedNode.item;
+    }
+
+    /**
+     * 获取链表中间位置的元素
+     *
+     * @return 链表中间位置上的元素
+     * @throws Exception 链表已空
+     */
+    public E getMiddle() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+        return getNode((size + 1) / 2).item;
+    }
+
+    /**
+     * 获取指定位置的节点
+     *
+     * @param idx 指定位置的索引
+     * @return 指定位置节点
+     * @throws Exception 超出链表范围
+     */
+    public Node<E> getNode(int idx) throws Exception
+    {
+        if (idx > size || idx < 0)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        Node<E> searchedNode;
+        if (idx <= size / 2)
+        {
+            searchedNode = header;
+            for (int i = 0; i < idx; i++)
+            {
+                searchedNode = searchedNode.next;
+            }
+        }
+        else
+        {
+            searchedNode = tailer;
+            for (int i = size + 1; i > idx; i--)
+            {
+                searchedNode = searchedNode.prev;
+            }
+        }
+
+        return searchedNode;
+    }
+
+    private static class Node<E>
+    {
+        E item;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(Node<E> prev, E e, Node<E> next)
+        {
+            this.prev = prev;
+            this.item = e;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        DoublyLinkedList2 doublyLinkedList2 = new DoublyLinkedList2();
+        doublyLinkedList2.addAsHead("c");
+        doublyLinkedList2.addAsHead("b");
+        doublyLinkedList2.addAsHead("a");
+        System.out.println(doublyLinkedList2.getNode(1).item);
+
+    }
+}
---
+++ linkedlist/SinglyLinkedList1.java
@@ -0,0 +1,279 @@
+package linkedlist;
+
+/**
+ * 单向链表
+ * <p>
+ * 在插入和删除元素时，涉及到边界时需要特殊处理。
+ *
+ * @param <E>
+ * @author wangzk
+ */
+public class SinglyLinkedList1<E>
+{
+    private int size = 0;
+    /**
+     * 头节点
+     */
+    private Node<E> head;
+    /**
+     * 尾节点
+     */
+    private Node<E> tail;
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    /**
+     * 前插一个元素
+     *
+     * @param e 待插元素
+     */
+    public void addAsHead(E e)
+    {
+        Node<E> newNode = new Node<>(e, head);
+        if (isEmpty())
+        {
+            tail = newNode;
+        }
+        head = newNode;
+        size++;
+    }
+
+    /**
+     * 追加一个元素
+     *
+     * @param e 待追加元素
+     */
+    public void addAsTail(E e)
+    {
+        Node<E> newNode = new Node(e, null);
+        if (isEmpty())
+        {
+            head = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    /**
+     * 在指定的位置插入新元素
+     *
+     * @param e   待插元素
+     * @param idx 待插入位置
+     */
+    public void addByIdx(E e, int idx) throws Exception
+    {
+        if (idx > size)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        if (idx == 1)
+        {
+            addAsHead(e);
+        }
+        else
+        {
+            // 找到插入位置的前驱节点
+            Node<E> prevNode = getNode(idx - 1);
+            // 创建新节点
+            Node<E> newNode = new Node<>(e, prevNode.next);
+            // 更新 prevNode 的指针
+            prevNode.next = newNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * 删除第一个元素
+     *
+     * @return 被删元素
+     * @throws Exception 链表已空
+     */
+    public E removeHead() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+
+        Node<E> oldHead = head;
+        if (size == 1)
+        {
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            head = oldHead.next;
+        }
+
+        size--;
+        return oldHead.item;
+    }
+
+    /**
+     * 删除最后一个元素
+     *
+     * @return 被删元素
+     * @throws Exception 链表已空
+     */
+    public E removeTail() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+
+        Node<E> oldTail = tail;
+        if (size == 1)
+        {
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            // 找到最后一个节点的前驱节点
+            Node<E> prevTail = getNode(size - 1);
+            // 将 prevTail 的后驱节点置空
+            prevTail.next = null;
+            // 更新 tail
+            tail = prevTail;
+        }
+
+        size--;
+        return oldTail.item;
+    }
+
+    /**
+     * 删除指定位置的元素
+     *
+     * @param idx 待删元素的索引
+     * @return 被删元素
+     */
+    public E removeByIdx(int idx) throws Exception
+    {
+        if (idx > size || idx < 1)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        E removed;
+        if (idx == 1)
+        {
+            removed = removeHead();
+        }
+        else if (idx == size)
+        {
+            removed = removeTail();
+        }
+        else
+        {
+            // 找到待删节点的前驱节点
+            Node<E> prevNode = getNode(idx - 1);
+            // 更新前驱节点的指针
+            prevNode.next = prevNode.next.next;
+            // 获取被删元素
+            removed = prevNode.next.item;
+            size--;
+        }
+
+        return removed;
+    }
+
+    /**
+     * 获取链表中间位置的元素
+     *
+     * @return 链表中间位置上的元素
+     * @throws Exception 链表已空
+     */
+    public E getMiddle() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+        return getNode((size + 1) / 2).item;
+    }
+
+    /**
+     * 链表反转
+     *
+     * @throws Exception 链表已空
+     */
+    public void reverse() throws Exception
+    {
+        if (isEmpty())
+        {
+            throw new Exception("链表已空！");
+        }
+
+        Node<E> prev = head;
+        Node<E> curr = head.next;
+        Node<E> temp;
+        while (prev != tail)
+        {
+            // 暂存 curr 的后继节点
+            temp = curr.next;
+            // 指针反转，将当前节点的指针指向前驱节点
+            curr.next = prev;
+            // 更新 prev 和 curr
+            prev = curr;
+            curr = temp;
+        }
+
+        // 头尾节点互换
+        temp = head;
+        head = tail;
+        tail = temp;
+
+        // 置空尾节点的指针
+        tail.next = null;
+    }
+
+    /**
+     * 获取指定位置的节点
+     *
+     * @param idx 指定位置的索引
+     * @return 指定位置节点
+     * @throws Exception 超出链表范围
+     */
+    public Node<E> getNode(int idx) throws Exception
+    {
+        if (idx > size || idx < 1)
+        {
+            throw new Exception("超出链表范围！");
+        }
+
+        Node<E> searchedNode = head;
+        for (int i = 0; i < idx - 1; i++)
+        {
+            searchedNode = searchedNode.next;
+        }
+        return searchedNode;
+    }
+
+    private static class Node<E>
+    {
+        E item;
+        Node<E> next;
+
+        Node(E item, Node<E> next)
+        {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
